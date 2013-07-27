12d393f2885eb32301193f524fc64111bf6a2c67
ojitha <ojithak@gmail.com>
2013-07-27 10:12:33 +1000

    updated for the "JAX-RS access via JSON using jQuery"

--- eafirstweb/src/main/java/au/com/blogspot/ojitha/eafirstweb/rest/DeptRestService.java
+++ eafirstweb/src/main/java/au/com/blogspot/ojitha/eafirstweb/rest/DeptRestService.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 
 import au.com.blogspot.ojitha.eafirstejb.DepartmentService;
@@ -66,5 +69,11 @@ public class DeptRestService {
     	return this.deptService.getAllDepartments();
     }
     
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Department> addDept( @FormParam("dept_Name") String deptName){
+    	this.deptService.save(deptName);
+    	return this.deptService.getAllDepartments();
+    }
 
 }
